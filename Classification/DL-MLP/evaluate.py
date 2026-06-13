307c0cf2b8db6e4f622d909757ea0459fc301100
Thunder1967 <pe950629@gmail.com>
2026-06-13 22:17:25 +0800

    update evaluate.py

--- Classification/DL-MLP/evaluate.py
+++ Classification/DL-MLP/evaluate.py
@@ -7,12 +7,18 @@ def getClassificationReport(data):
 def unclassifiedRate(all_data,unclassified_data):
     return len(unclassified_data)/len(all_data)
 
-def NDIUD(unclassified_data):
+def newClassPrecision(unclassified_data):
     # new data in unclassified data
     missing_in_train = ['BARBUNYA', 'BOMBAY']
     new_class_count = unclassified_data['Class'].isin(missing_in_train).sum()
     total_unclassified = len(unclassified_data)
     return new_class_count/total_unclassified
+
+def newClassRecall(all_data,unclassified_data):
+    missing_in_train = ['BARBUNYA', 'BOMBAY']
+    new_class_in_unclassified = unclassified_data['Class'].isin(missing_in_train).sum()
+    total_new_class = all_data['Class'].isin(missing_in_train).sum()
+    return new_class_in_unclassified/total_new_class
 
 def printAllResult(all_data,classified_data,unclassified_data):
     print("=== All Data Report ===")
@@ -21,7 +27,11 @@ def printAllResult(all_data,classified_data,unclassified_data):
     print(getClassificationReport(classified_data))
     print("=== unclassified data rate ===")
     print(unclassifiedRate(all_data,unclassified_data))
-    print("=== new data in unclassified data ===")
-    print(NDIUD(unclassified_data))
-    print("=== new data in unclassified data * unclassified data rate ===")
-    print(NDIUD(unclassified_data)*unclassifiedRate(all_data,unclassified_data))
+    print("=== # of unclassified data ===")
+    print(len(unclassified_data))
+    print("=== new Class Precision ===")
+    print(a:=newClassPrecision(unclassified_data))
+    print("=== new Class Recall  ===")
+    print(b:=newClassRecall(all_data,unclassified_data))
+    print("=== F1-score  ===")
+    print(2*(a*b)/(a+b))
