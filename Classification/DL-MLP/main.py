b4754f26eb6f05d74ff9904973df43852f75bc22
Thunder1967 <pe950629@gmail.com>
2026-06-14 16:09:46 +0800

    update

--- Classification/DL-MLP/main.py
+++ Classification/DL-MLP/main.py
@@ -8,7 +8,7 @@ import myUtil
 def case1():
     model = normal_model.normal_model_package(from_file="Classification\\DL-MLP\\normal_model.pth")
     model.load_test_data()
-    all_data,classified_data,unclassified_data = model.testingMCDropout()
+    all_data,classified_data,unclassified_data = model.testingEnergy(threshold=-2.0,T=1)
     evaluate.printAllResult(all_data,classified_data,unclassified_data)
     return all_data,classified_data,unclassified_data
 
@@ -22,17 +22,18 @@ def case2():
 def case3():
     model = normal_model_with_IsolationForest.mix_model_package(from_file="Classification\\DL-MLP\\mix_model.pth")
     model.load_test_data()
-    all_data,classified_data,unclassified_data = model.testingEnergy()
+    all_data,classified_data,unclassified_data = model.testingMCDropout()
     evaluate.printAllResult(all_data,classified_data,unclassified_data)
     return all_data,classified_data,unclassified_data
 
 def case4():
     model = CRF_model.CRF_model_package(from_file="Classification\\DL-MLP\\CRF_model.pth")
     model.load_test_data()
-    all_data,classified_data,unclassified_data = model.testingEnergy()
+    all_data,classified_data,unclassified_data = model.testingMCDropout(threshold=0.3)
     evaluate.printAllResult(all_data,classified_data,unclassified_data)
     return all_data,classified_data,unclassified_data
 
 if __name__=="__main__":
     # normal_model_with_IsolationForest.mix_model_package().saveModelPackage()
-    myUtil.outputResultToFile(*case4())
+    # myUtil.outputResultToFile(*case4())
+    case4()
